1d81618ddaa3cbfc21fa75754c37cb7da01d063e
Victor Freitas <vitoralves59@gmail.com>
2023-04-04 15:14:09 -0300

    Release 1.0.3

---
+++ Example/UICodeKit/Sections/Home/Controller/HomeController.swift
@@ -0,0 +1,33 @@
+//
+//  HomeController.swift
+//  UICodeKit_Example
+//
+//  Created by Victor Freitas on 04/04/23.
+//  Copyright © 2023 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+ 
+    // MARK: View
+    
+    let rootView = HomeView()
+    override func loadView() {
+        view = rootView
+    }
+    
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        registerActions()
+    }
+    
+    // MARK: Actions
+    
+    func registerActions() {
+        rootView.loginTapped = {
+            print("Loggin Tapped!")
+        }
+    }
+}
